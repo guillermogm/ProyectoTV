0fe4c5e8c569b998453014bcdd5938407289b921
guillermogm <guillermogm009@gmail.com>
2024-06-09 00:30:33 +0200

    Feat: Finishing channel buttons and update of info button

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ let pantalla = document.getElementById("pantalla");
 //Fecha
 const currentDate = new Date();
 //Formateo de la fecha
-const options = { 
-  weekday: 'long', 
-  year: 'numeric', 
-  month: 'long', 
-  day: 'numeric' 
+const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
 };
 
 let arrayBotones = Array.from(botones);
@@ -20,23 +20,16 @@ power.addEventListener("click", () => {
     interruptor = !interruptor;
     if (interruptor) {
         pantalla.src = "../img/Static.gif"
-        pantalla.style.display="inline"
+        pantalla.style.display = "inline"
     } else {
         pantalla.src = ""; pantalla.style.display = "none";
         mensaje.innerHTML = "";
     }
 })
-//Contador para apagar el info y boton info
-let contador=2;
-info.addEventListener("click", () => {
-    if (interruptor) {
-        contador+=1;
-        mensaje.innerHTML=currentDate.toLocaleDateString('es-ES', options)
-        if(contador%2 == 0){
-           mensaje.innerHTML="" 
-        }
-    }
-})
+// Canal en el que estas
+let ncanal
+// Variable de contro info
+let control
 //Recorrer los botones y cambiar de canales
 for (let i = 0; i < arrayBotones.length; i++) {
     arrayBotones[i].addEventListener("click", () => {
@@ -44,16 +37,67 @@ for (let i = 0; i < arrayBotones.length; i++) {
             switch (i) {
                 case 0:
                     pantalla.src = "../img/Simpson.gif";
+                    ncanal = 1;
+                    control = ncanal;
                     break;
                 case 1:
-                    pantalla.src = "../img/Static.gif"
+                    pantalla.src = "../img/Bojack.gif";
+                    ncanal = 2;
+                    control = ncanal;
                     break;
                 case 2:
-                    pantalla.src = "../img/Simpson.gif";
+                    pantalla.src = "../img/BreakingBad.gif";
+                    ncanal = 3;
+                    control = ncanal;
                     break;
+                case 3:
+                    pantalla.src = "../img/Csi.gif";
+                    ncanal = 4;
+                    control = ncanal;
+                    break;
+                case 4:
+                    pantalla.src = "../img/Friends.gif";
+                    ncanal = 5;
+                    control = ncanal;
+                    break;
+                case 5:
+                    pantalla.src = "../img/Game.gif";
+                    ncanal = 6;
+                    control = ncanal;
+                    break;
+                case 6:
+                    pantalla.src = "../img/Peaky.gif";
+                    ncanal = 7;
+                    control = ncanal;
+                    break;
+                case 7:
+                    pantalla.src = "../img/RickMorty.gif";
+                    ncanal = 8;
+                    control = ncanal;
+                    break;
+                case 8:
+                    pantalla.src = "../img/Tom.gif";
+                    ncanal = 9;
+                    control = ncanal;
+                    break;
+
             }
 
         }
     })
 
 }
+// Boton info
+info.addEventListener("click", () => {
+    if (interruptor) {
+        //Intervalo en el que se muestra la info
+        let intervalId = setInterval(function () {
+            mensaje.innerHTML = "Canal " + ncanal + " " + currentDate.toLocaleDateString('es-ES', options)
+
+        }, 0);
+        setTimeout(function () {
+            clearInterval(intervalId);
+            mensaje.innerHTML="";
+        }, 1200);
+    }
+})
