e24f8f2ac472c2ebbfa82274f0c910ede41e06f6
guillermogm <guillermogm009@gmail.com>
2024-06-08 23:20:27 +0200

    feat: Info button with the date formated

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 let botones = document.getElementsByClassName("button-number");
-let power = document.getElementById("power");
+let power = document.getElementById("power-button");
 let info = document.getElementById("info");
 let interruptor = false;
 let pantalla = document.getElementById("pantalla");
+//Fecha
+const currentDate = new Date();
+//Formateo de la fecha
+const options = { 
+  weekday: 'long', 
+  year: 'numeric', 
+  month: 'long', 
+  day: 'numeric' 
+};
 
 let arrayBotones = Array.from(botones);
+
 //power button
 power.addEventListener("click", () => {
     interruptor = !interruptor;
@@ -13,6 +23,17 @@ power.addEventListener("click", () => {
         pantalla.style.display="inline"
     } else {
         pantalla.src = ""; pantalla.style.display = "none";
-        info.innerHTML = "";
+        mensaje.innerHTML = "";
+    }
+})
+//Contador para apagar el info
+let contador=2;
+info.addEventListener("click", () => {
+    if (interruptor) {
+        contador+=1;
+        mensaje.innerHTML=currentDate.toLocaleDateString('es-ES', options)
+        if(contador%2 == 0){
+           mensaje.innerHTML="" 
+        }
     }
 })
